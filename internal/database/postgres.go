49f51c8b3f6d06263ea3159cd518848a8a1cb65d
abdwhidd <abdulwahidkaharr@gmail.com>
2026-04-23 20:19:14 +0700

    feat: connect to PostgreSQL database

---
+++ internal/database/postgres.go
@@ -0,0 +1,31 @@
+package database
+
+import (
+	"database/sql"
+	"fmt"
+	"os"
+
+	_ "github.com/lib/pq"
+)
+
+func NewPostgresDB() (*sql.DB, error) {
+	dsn := fmt.Sprintf(
+		"postgres://%s:%s@%s:%s/%s?sslmode=disable",
+		os.Getenv("DB_USER"),
+		os.Getenv("DB_PASSWORD"),
+		os.Getenv("DB_HOST"),
+		os.Getenv("DB_PORT"),
+		os.Getenv("DB_NAME"),
+	)
+
+	db, err := sql.Open("postgres", dsn)
+	if err != nil {
+		return nil, err
+	}
+
+	if err := db.Ping(); err != nil {
+		return nil, err
+	}
+
+	return db, nil
+}
--- main.go
+++ main.go
@@ -1,11 +1,30 @@
 package main
 
 import (
+	"auth-api/internal/database"
 	"auth-api/internal/handler"
+	"fmt"
 	"net/http"
+
+	"github.com/joho/godotenv"
 )
 
 func main() {
+
+	if err := godotenv.Load(); err != nil {
+		fmt.Println("error loading env")
+	}
+
+	db, err := database.NewPostgresDB()
+	if err != nil {
+		fmt.Println("error connecting to database:", err)
+	}
+
+	if err == nil {
+		fmt.Println("Connected to PostgreSQL database successfully!")
+	}
+	defer db.Close()
+
 	authHandler := handler.NewAuthHandler()
 	http.HandleFunc("/register", authHandler.RegisterHandler)
 	http.ListenAndServe(":8080", nil)
